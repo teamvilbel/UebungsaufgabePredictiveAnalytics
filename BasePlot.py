b7aa043b610ead819fda12207f305cca0b8cf198
SirMoM <noah.ruben@online.de>
2019-10-28 11:36:24 +0100

    Show missing daten matrix

--- BasePlot.py
+++ BasePlot.py
@@ -2,9 +2,13 @@ import math
 
 import pandas as pd
 from matplotlib import pyplot
+import missingno as msno
 
 # csv in einen dataframe einlesen
 ts = pd.read_csv("19.06.20_travels_Frankfurt.csv", header=0, index_col=0)
+
+msno.matrix(ts)
+pyplot.show()
 
 # Spalte mit Passagieren extrahieren und in eine Liste konvertieren
 TAc = ts["TAc"]
