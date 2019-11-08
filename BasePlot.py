1684e772ceb5683bc7d9c2abdce9fcc22bd42f51
SirMoM <noah.ruben@online.de>
2019-11-08 12:51:32 +0100

    some stuff

--- BasePlot.py
+++ BasePlot.py
@@ -4,17 +4,65 @@ import pandas as pd
 from matplotlib import pyplot
 import missingno as msno
 
+# CSV Collums TAA,TA,TIN,TIR,TSI,TIM,TIL,TIRE,TIP,TIT,TID,TSC,TAc
+columns = ["TAA", "TA", "TIN", "TIR", "TSI", "TIM", "TIL", "TIRE", "TIP", "TIT", "TID", "TSC", "TAc"]
+
+# TAA Alerts (@@ separated)
+alertC = "TAA"
+
+# TA Delay hour (before was in minutes) need to be compared with TIT column
+delayHourC = "TA"
+
+# TIN Train Model
+trainModelC = "TIN"
+
+# TIR Route
+routeC = "TIR"
+
+# TSI Station ID
+stationIdC = "TSI"
+
+# TIM Direction departura/arrival ==> can be converted to numbers
+directionC = "TIM"
+
+# TIL (http) Request with parameters
+requestC = "TIL"
+
+# TIRE Destination
+destinationC = "TIRE"
+
+# TIP Platform number
+platformNumberC = "TIP"
+
+# TIT Departure hour
+departureHourC = "TIT"
+
+# TID Date
+dateC = "TID"
+
+# TSC Station Name and ID
+stationNameAndIdC = "TSC"
+
+# TAc Delay in minutes ( TA - TIT, delayHourC - depatureHourC)
+delayInMinutesC = "TAc"
+
 # csv in einen dataframe einlesen
-ts = pd.read_csv("19.06.20_travels_Frankfurt.csv", header=0, index_col=0)
+df = pd.read_csv("19.06.20_travels_Frankfurt.csv", header=0, index_col=0, na_values=[""])
+
+# print("x ", df[delayHourC])
 
 # Show missing data as matrix
-msno.matrix(ts)
+msno.matrix(df)
+pyplot.show()
+
+# Show missing data as bar-diagramm
+msno.bar(df, color="green")
 pyplot.show()
 
 # Spalte mit TAc extrahieren und in eine Liste konvertieren
-TAc = ts["TAc"]
+TAc = df["TAc"]
 TAcList = TAc.values.tolist()
+print(TAcList)
 
-pyplot.plot(TAcList)
+pyplot.scatter(TAcList, TAcList)
 pyplot.show()
-
