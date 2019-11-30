3280c5217a58a7745ce155bdb6c4fc0a0cf9debc
SirMoM <noah.ruben@online.de>
2019-11-30 15:36:08 +0100

    Update Readme

--- BasePlot.py
+++ BasePlot.py
@@ -3,6 +3,9 @@ import math
 import pandas as pd
 from matplotlib import pyplot
 import missingno as msno
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import numpy as np  # linear algebra
 
 # CSV Collums TAA,TA,TIN,TIR,TSI,TIM,TIL,TIRE,TIP,TIT,TID,TSC,TAc
 columns = ["TAA", "TA", "TIN", "TIR", "TSI", "TIM", "TIL", "TIRE", "TIP", "TIT", "TID", "TSC", "TAc"]
@@ -47,28 +50,56 @@ stationNameAndIdC = "TSC"
 delayInMinutesC = "TAc"
 
 # csv in einen dataframe einlesen
-df = pd.read_csv("19.06.20_travels_Frankfurt.csv", header=0, index_col=0, na_values=[""])
+# df = pd.read_csv("19.06.20_travels_Frankfurt.csv", header=0, index_col=0, na_values=[""])
+'''
+0 TAA   Nicht relevant
+1 TA    Nur für die Berechnung fehlender Werte 
+2 TIN   One-Hot-Encoding für die Linien (BSP.)
+3 TIR   Nur relevant für erweiterte Auswertungen (Verspätungen anhand von Zwichenstationen)
+4 TSI   Nicht relevant da alle Daten von uns immer von Frankfurt aus sind
+5 TIM   One-Hot-Encoding für die arr = 0 dep = 1(BSP.)
+6 TIL   Nicht relevant denke ich (HELP WANTED)
+7 TIRE  One-Hot-Encoding für die Bahnhöfe (BSP.)
+8 TIP   Entfernen vom ORT Gleisnummer / ID ist aussreichen ⇒ nur Gleise in FFM
+9 TIT
+10 TID
+11 TSC  Nicht relevant da alle Daten von uns immer von Frankfurt aus sind
+12 TAc  Fehler-Findung  / Neuberechnung
+'''
+df = pd.read_csv("19.06.20_travels_Frankfurt.csv",
+                 header=0,
+                 usecols=[0, 1, 2, 5, 7, 8, 9, 10, 11, 12],
+                 parse_dates=["TID"],
+                 na_values=[''],
+                 names=columns
+                 );
 
- # löschen der ungültigen DS
-df = df[dataset[delayInMinutesC] < -1]
+df.dataframeName = "19.06.20_travels_Frankfurt.csv";
+# Vor dem Preprocessing
+nRow, nCol = df.shape;
+print("Befor preprocessing:");
+print(f'There are {nRow} rows and {nCol} columns');
+
+# Format Times ?
+# df['TA'] = pd.to_datetime(df['TA'], format='%H:%M:%S').dt.time
+# df['TIT'] = pd.to_datetime(df['TIT'], format='%H:%M:%S').dt.time
+
+# löschen der ungültigen DS
+df[delayInMinutesC] = df[delayInMinutesC].astype(int);
+df[delayInMinutesC] = df[df[delayInMinutesC] >= 0];
 
 # löschen der ausgefallenen Fahrten
-df = df[dataset[alertC] != ' Fahrt fällt aus'] 
+# df = df[df["TAA"] != ' Fahrt fällt aus'];
+df = df[df.TAA != ' Fahrt fällt aus'];
+df.drop('TAA', axis=1);
 
-# print("x ", df[delayHourC])
 
-# Show missing data as matrix
-msno.matrix(df)
-pyplot.show()
 
-# Show missing data as bar-diagramm
-msno.bar(df, color="green")
-pyplot.show()
+nRow, nCol = df.shape;
+print("After preprocessing:");
+print(f'There are {nRow} rows and {nCol} columns');
+print(df)
+# plotPerColumnDistribution(df)
 
-# Spalte mit TAc extrahieren und in eine Liste konvertieren
-TAc = df["TAc"]
-TAcList = TAc.values.tolist()
-print(TAcList)
-
-pyplot.scatter(TAcList, TAcList)
-pyplot.show()
+df.boxplot();
+pyplot.show();
